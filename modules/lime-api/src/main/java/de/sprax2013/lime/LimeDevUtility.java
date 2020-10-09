f4d435525e72611e8a3c2f5caefd62784d7fac59
Christian Koop <developer@sprax2013.de>
2020-10-09 09:20:46 +0200

    Code cleanup

--- modules/lime-api/src/main/java/de/sprax2013/lime/LimeDevUtility.java
+++ modules/lime-api/src/main/java/de/sprax2013/lime/LimeDevUtility.java
@@ -4,4 +4,8 @@ import java.util.logging.Logger;
 
 public class LimeDevUtility {
     public static final Logger LOGGER = Logger.getLogger("LimeDevUtility");
+
+    private LimeDevUtility() {
+        throw new IllegalStateException("Utility class");
+    }
 }
