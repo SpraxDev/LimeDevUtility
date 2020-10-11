a2d0a3cfe26a81ac8906d560064e7ad9dcf6470c
Christian Koop <developer@sprax2013.de>
2020-10-11 17:42:01 +0200

    Introduce BungeeCord support
    * Create 'lime-bungeecord-api' module
    * Prepare 'lime-bungeecord' to be loaded as a normal plugin
    * Fix bStats to work with Spigot and BungeeCord (they need to use different plugin ids)

--- modules/bungeecord/lime-bungeecord/src/main/java/de/sprax2013/lime/bungeecord/BungeeLime.java
+++ modules/bungeecord/lime-bungeecord/src/main/java/de/sprax2013/lime/bungeecord/BungeeLime.java
@@ -3,4 +3,8 @@ package de.sprax2013.lime.bungeecord;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class BungeeLime extends Plugin {
+    @Override
+    public void onEnable() {
+        LimeDevUtilityBungee.init(this);
+    }
 }
